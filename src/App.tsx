e4876ddbc849971e1ac3cdf519c83b3a12af36db
ProgrammingGeek02 <davideveriwhere@gmail.com>
2021-08-28 19:02:34 -0500

    Initializing the client

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function App() {
+  return (
+    <>
+      <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4s">
+        <div className="flex-shrink-0">
+
+        </div>
+        <div>
+          <div className="text-xl font-medium text-black">
+            ChitChat
+          </div>
+          <p className="text-gray-500">
+            You have a new message!
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
